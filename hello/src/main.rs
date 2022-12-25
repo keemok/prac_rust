c11662bbd805964951ef8a38abac1447fb731ce0
keemok <keemok@naver.com>
2022-12-26 00:45:02 +0900

    modify main.rs

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
-    println!("Hello, world!");
+    greet_world();
+}
+
+fn greet_world(){
+    let america = "Hello, world!";
+    let korea ="안녕하세요.";
+    let regions = [america, korea];
+
+    for region in regions.iter(){
+        println!("{}",&region);
+    }
 }
