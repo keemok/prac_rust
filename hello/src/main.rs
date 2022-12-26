4596c70afc5d098c1239ded062d0e2234fa8a95c
keemok <keemok@naver.com>
2022-12-26 22:15:22 +0900

    Init

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -1,13 +1,5 @@
 fn main() {
-    greet_world();
+    println!("hello world");
 }
 
-fn greet_world(){
-    let america = "Hello, world!";
-    let korea ="안녕하세요.";
-    let regions = [america, korea];
 
-    for region in regions.iter(){
-        println!("{}",&region);
-    }
-}
