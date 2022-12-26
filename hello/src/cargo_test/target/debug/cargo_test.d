2ebc2f08d1c1600bcbf3f06b04cb3c4053d4e4bd
keemok <keemok@naver.com>
2022-12-26 23:15:38 +0900

    1week

---
+++ hello/src/cargo_test/target/debug/cargo_test.d
@@ -0,0 +1,1 @@
+C:\language\hello\src\cargo_test\target\debug\cargo_test.exe: C:\language\hello\src\cargo_test\src\main.rs
---
+++ hello/src/cargo_test/target/debug/deps/cargo_test.d
@@ -0,0 +1,5 @@
+C:\language\hello\src\cargo_test\target\debug\deps\cargo_test.exe: src\main.rs
+
+C:\language\hello\src\cargo_test\target\debug\deps\cargo_test.d: src\main.rs
+
+src\main.rs:
